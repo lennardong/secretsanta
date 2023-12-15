e3db215e2b766693859f9dcfe8223621529eb7c3
lennardong <lennard.ong@gmail.com>
2023-12-15 22:12:32 +0800

    Minor refactor

--- backend/src/stack.py
+++ backend/src/stack.py
@@ -8,21 +8,6 @@ import random
 from pprint import pprint
 from typing import Dict, List, Tuple, Optional, Union
 
-names = {
-    "Clive": ["Alma", "Amy"],
-    "Alma": ["Clive", "Amy"],
-    "Andrew": ["Emma", "Evelyn", "William"],
-    "Emma": ["Andrew", "Evelyn", "William"],
-    "Lennard": ["Lara", "Leah", "Maya"],
-    "Lara": ["Lennard", "Leah", "Maya"],
-    "Wayne": ["Amy"],
-    "Amy": ["Wayne", "Clive", "Alma"],
-    "Annie": [],
-    "Leah": ["Lennard", "Lara", "Maya"],
-    "Maya": ["Lennard", "Lara", "Leah"],
-    "William": ["Andrew", "Emma", "Evelyn"],
-    "Evelyn": ["Andrew", "Emma", "William"],
-}  # {gifter: [gifteesToAvoid]}
 
 def generate_secretSanta(
         names: Dict[str, List[str]]
@@ -49,10 +34,27 @@ def generate_secretSanta(
     
     return result
 
-result = generate_secretSanta(names)
-pprint(result)
+if __name__ == "__main__":
+    names = {
+        "Clive": ["Alma", "Amy"],
+        "Alma": ["Clive", "Amy"],
+        "Andrew": ["Emma", "Evelyn", "William"],
+        "Emma": ["Andrew", "Evelyn", "William"],
+        "Lennard": ["Lara", "Leah", "Maya"],
+        "Lara": ["Lennard", "Leah", "Maya"],
+        "Wayne": ["Amy"],
+        "Amy": ["Wayne", "Clive", "Alma"],
+        "Annie": [],
+        "Leah": ["Lennard", "Lara", "Maya"],
+        "Maya": ["Lennard", "Lara", "Leah"],
+        "William": ["Andrew", "Emma", "Evelyn"],
+        "Evelyn": ["Andrew", "Emma", "William"],
+    }  # {gifter: [gifteesToAvoid]}
+    
+    result = generate_secretSanta(names)
+    pprint(result)
 
-for gifter, giftee in result:
-    print(f"🥷Your Secret Santa: {gifter} -> {giftee} (👍 to accept)")
+    for gifter, giftee in result:
+        print(f"🥷Your Secret Santa: {gifter} -> {giftee} (👍 to accept)")
 
 
